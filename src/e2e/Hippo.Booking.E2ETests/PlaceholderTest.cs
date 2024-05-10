d6dc35208eebe99bb1adefada63a9418dded093a
adamclarkson <adam.clarkson@education.gov.uk>
2024-05-10 17:33:40 +0100

    chore: added e2e test project

---
+++ src/e2e/Hippo.Booking.E2ETests/PlaceholderTest.cs
@@ -0,0 +1,15 @@
+namespace Hippo.Booking.E2ETests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
