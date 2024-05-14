41e5e639f32107f548b9d70001bf71677ba666a2
Harry Young <harry.young@hippodigital.co.uk>
2024-05-14 18:43:58 +0100

    Add pulumi azure project

---
+++ src/infrastructure/Hippo.Booking.Deploy.Azure/Program.cs
@@ -0,0 +1,10 @@
+﻿using Pulumi.Azure.Core;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    var rg = new ResourceGroup("booking-rg", new ResourceGroupArgs
+    {
+        Name = "booking-rg",
+        Location = "West Europe"
+    });
+});
