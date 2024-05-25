79bf85ccd7c84d14e44ea2e4be2c7d6d722bf42e
Harry Young <harry.young@hippodigital.co.uk>
2024-05-25 17:48:25 +0100

    Add SQL to Pulumi

--- src/infrastructure/Hippo.Booking.Deploy.Azure/Program.cs
+++ src/infrastructure/Hippo.Booking.Deploy.Azure/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Pulumi;
 using Pulumi.Azure.AppService;
 using Pulumi.Azure.AppService.Inputs;
-using Pulumi.Azure.Core;
+using Pulumi.Azure.Core;∂
+using Pulumi.Azure.Sql;
+using Pulumi.Random;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
@@ -17,6 +19,39 @@ return await Pulumi.Deployment.RunAsync(() =>
         Location = "West Europe"
     });
     
+    var frontEnd = new StaticWebApp("hippo-booking-static-web", new StaticWebAppArgs
+    {
+        Name = WithStackName("hippo-booking-static-web"),
+        ResourceGroupName = rg.Name,
+        Location = rg.Location,
+        SkuTier = "Free"
+    });
+    
+    var randomDbPassword = new RandomPassword("hippo-booking-db-password", new RandomPasswordArgs
+    {
+        Length = 16,
+        Special = true
+    });
+    
+    var sqlServer = new SqlServer("hippo-booking-sql-server", new SqlServerArgs
+    {
+        Name = WithStackName("hippo-booking-sql-server"),
+        ResourceGroupName = rg.Name,
+        Location = rg.Location,
+        Version = "12.0",
+        AdministratorLogin = "admin",
+        AdministratorLoginPassword = randomDbPassword.Result
+    });
+    
+    var sqlDatabase = new Database("hippo-booking-sql-database", new DatabaseArgs
+    {
+        Name = WithStackName("hippo-booking-sql-database"),
+        ResourceGroupName = rg.Name,
+        Location = rg.Location,
+        ServerName = sqlServer.Name,
+        RequestedServiceObjectiveName = "S0"
+    });
+    
     var backEndAppServicePlan = new ServicePlan("hippo-booking-back-end-plan", new ServicePlanArgs
     {
         Name = WithStackName("hippo-booking-asp"),
@@ -25,6 +60,14 @@ return await Pulumi.Deployment.RunAsync(() =>
         SkuName = "B1",
         OsType = "Linux"
     });
+
+    var connectionString = Output.Tuple(sqlServer.Name, sqlDatabase.Name, sqlServer.AdministratorLoginPassword)
+        .Apply(t =>
+        {
+            var (serverName, databaseName, password) = t;
+            return
+                $"Server=tcp:{serverName}.database.windows.net;initial catalog={databaseName};user ID={sqlServer.AdministratorLogin};password={password};Min Pool Size=0;Max Pool Size=30;Persist Security Info=True;";
+        });
     
     var backEndAppService = new LinuxWebApp("hippo-booking-app-service", new LinuxWebAppArgs
     {
@@ -36,19 +79,12 @@ return await Pulumi.Deployment.RunAsync(() =>
         {
             { "DOCKER_REGISTRY_SERVER_URL", "ghcr.io" },
             { "DOCKER_REGISTRY_SERVER_USERNAME" , config.Require("DockerUsername")},
-            { "DOCKER_REGISTRY_SERVER_PASSWORD", config.RequireSecret("DockerPassword")}
+            { "DOCKER_REGISTRY_SERVER_PASSWORD", config.RequireSecret("DockerPassword")},
+            { "ConnectionStrings__HippoBookingDbContext", connectionString}
         },
         SiteConfig = new LinuxWebAppSiteConfigArgs
         {
             AlwaysOn = true
         }
-    });
-    
-    var frontEnd = new StaticWebApp("hippo-booking-static-web", new StaticWebAppArgs
-    {
-        Name = WithStackName("hippo-booking-static-web"),
-        ResourceGroupName = rg.Name,
-        Location = rg.Location,
-        SkuTier = "Free"
     });
 });
