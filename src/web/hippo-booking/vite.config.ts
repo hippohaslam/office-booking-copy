645cc39a3494242417bc4ec4f955d36a8b333cd8
Harry Young <harry.young@hippodigital.co.uk>
2024-06-14 11:14:49 +0100

    Get external environment variables

--- src/web/hippo-booking/vite.config.ts
+++ src/web/hippo-booking/vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["test/vitest.setup.ts"]
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 5173
+export default defineConfig(({ command, mode }) => {
+  const externalEnvs = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      externalEnvs
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["test/vitest.setup.ts"],
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 5173
+    }
   }
 })
