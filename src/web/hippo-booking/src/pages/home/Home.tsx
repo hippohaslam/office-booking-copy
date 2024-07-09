78b343fe0021d6099c7cee79cdc00597b7d03dd0
ClarksonAdam <adam.clarkson@hippodigital.co.uk>
2024-07-09 16:10:04 +0100

    fix: fixed height issues on mobile

--- src/web/hippo-booking/src/pages/home/Home.tsx
+++ src/web/hippo-booking/src/pages/home/Home.tsx
@@ -22,6 +22,10 @@ const Home = () => {
               <div className="content-container">
                   <h2>Our offices</h2>
                   <p>Some content about our offices will go in here.</p>
+                  <h3>Some more content</h3>
+                  <p>Filling this page with more content to force the height to go beyond 100vh on mobile devices</p>
+                  <h4>Why</h4>
+                  <p>Because safari on mobile doesn't respect 100vh with it's fancy floating search bar</p>
               </div>
           </section>
       </div>
