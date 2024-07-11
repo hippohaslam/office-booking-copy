de9730f53b18459e06217ba426943f1c96a7cf0b
ClarksonAdam <adam.clarkson@hippodigital.co.uk>
2024-07-11 12:23:29 +0100

    fix: removed unused import

--- src/web/hippo-booking/src/App.tsx
+++ src/web/hippo-booking/src/App.tsx
@@ -1,6 +1,5 @@
 import "./styles/App.scss";
 import { Outlet } from "react-router-dom";
-// import Nav from "./components/nav/Nav";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useAxiosInterceptors from "./hooks/AxiosInterceptor";
 
