91d45586e265f5682036d5aca343d24c889b6988
Stuart Maskell <st.mas29@gmail.com>
2024-07-22 13:03:12 +0100

    Home page update (#26)
    * use react rendering state and set min height on hero to help with jarring
    
    * upd

--- src/web/hippo-booking/src/pages/home/Home.tsx
+++ src/web/hippo-booking/src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useUser } from "../../contexts/UserContext.tsx";
 import RelaxingGraphic from '../../assets/undraw_a_moment_to_relax_re_v5gv.svg';
 import OfficeGraphic from '../../assets/undraw_in_the_office_re_jtgc.svg'
@@ -8,52 +9,67 @@ import { getUpcomingBookingsAsync } from "../../services/Apis.ts";
 import ErrorBanner from "../../components/banners/ErrorBanner.tsx";
 import BookingCardStacked from "../../components/booking/BookingCardStacked.tsx";
 
+
 const Home = () => {
     const userContext = useUser();
+    const [graphicSrc, setGraphicSrc] = useState(RelaxingGraphic);
 
-    const { isFetching, error, data } = useQuery({
+    const { isFetching, data, isSuccess, isError } = useQuery({
         queryKey: ["bookings"],
         queryFn: getUpcomingBookingsAsync,
     });
 
-    const nextBooking = data?.at(0);
-    let card;
-    let graphicSrc;
+    // Set the graphic based on the state of the query
+    useEffect(() => {
+        if(isFetching) {
+            setGraphicSrc(RelaxingGraphic);
+        } else if (isError) {
+            setGraphicSrc(ErrorGraphic);
+        } else if(isSuccess && data?.length === 0) {
+            setGraphicSrc(RelaxingGraphic);
+        } else if (isSuccess) {
+            setGraphicSrc(OfficeGraphic);
+        }
+    }, [data?.length, isFetching, isSuccess ,isError]);
 
-    if (isFetching) {
-        graphicSrc = RelaxingGraphic;
-        card = <div><p>Fetching bookings...</p></div>;
-    }
-    if (error) {
-        graphicSrc = ErrorGraphic;
-        card = <div><ErrorBanner text="Unable to get bookings, please refresh the page" /></div>;
-    }
-    if (data?.length === 0) {
-        graphicSrc = RelaxingGraphic;
-        card = <div><p>You don't have any upcoming bookings.</p></div>;
-    }
-    else { 
-        graphicSrc = OfficeGraphic;
-        card = 
-            <div>
-                <p>You have an upcoming booking:</p>
-                <BookingCardStacked                 
-                    date={nextBooking?.date as Date}
-                    bookableObjectName={nextBooking?.bookableObject.name as string}
-                    areaName={nextBooking?.area.name as string}
-                    locationName={nextBooking?.location.name as string}
-                    />
-                <Link className='with-arrow' to="/locations">Manage this and other bookings</Link>
-            </div>;
-    }
+    // Just returns HTML based on the state of the query
+    const cardToShow = () => {
+        if (isFetching) {
+            return <p>Fetching bookings...</p>;
+        }
+        else if (isError) {
+            return <ErrorBanner text="Unable to get bookings, please refresh the page" />;
+        }
+        
+        else if(isSuccess && data) {
+            const nextBooking = data.at(0);
+            if(data.length === 0 || !nextBooking) {
+                return <p>You don't have any upcoming bookings.</p>;
+            }
+            
+            return <BookingCardStacked                 
+                date={nextBooking.date as Date}
+                bookableObjectName={nextBooking.bookableObject.name as string}
+                areaName={nextBooking.area.name as string}
+                locationName={nextBooking.location.name as string}
+                />;
+        } else {
+            // This should never happen
+            return <p></p>;
+        }
+    };
 
     return (
         <div className="page-container">
             <section className="full-width-navy">
                 <div className="hero-container">
-                    <div className="hero-content">
+                    <div className="hero-content hero-container__medium-height">
                         <h1>Hi {userContext.user?.firstName}</h1>
-                        {card}
+                        {isSuccess ? (
+                            <div>
+                                {cardToShow()}
+                            </div>
+                        ) : null}
                         <Link to="/locations" className="cta cta-pink with-arrow">Make a new booking</Link>
                     </div>
                     <img className="hero-graphic" alt="" src={graphicSrc}/>
