8b2bfbbde0af6a37493285077006b46777096a1d
ClarksonAdam <adam.clarkson@hippodigital.co.uk>
2024-07-26 17:50:38 +0100

    test: added bookable object int tests

---
+++ src/api/tests/Hippo.Booking.Integration.Tests/Tests/BookableObjectEndpointTests.cs
@@ -0,0 +1,114 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Hippo.Booking.Application.Commands.BookableObject;
+using Hippo.Booking.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hippo.Booking.Integration.Tests.Tests;
+
+public class BookableObjectEndpointTests : IntegrationTestBase
+{
+    [Test]
+    public async Task CreatingAUniqueBookableObjectForAnExistingAreaAndLocationIsSuccessful()
+    {
+        //Arrange
+        var client = GetClient();
+        var location = await SetUpLocation("Bookable Object Test Location 1");
+        var area = await SetUpArea(location, "Bookable Object Test Area 1");
+
+        var createBookableObjectRequest = new CreateBookableObjectRequest
+        {
+            Name = "Bookable Object Test Object 1",
+            Description = "Bookable Object Test Object 1",
+            FloorPlanObjectId = "1"
+        };
+        
+        //Act
+        var response = await client.PostAsJsonAsync($"location/{location.Id}/area/{area.Id}/bookable-object",
+            createBookableObjectRequest);
+        
+        //Assert
+        response.EnsureSuccessStatusCode();
+        
+        var bookableObjectId = response.Headers.Location!.ToString().Split('/').Last();
+
+        var dbObject = DbContext.BookableObjects.SingleOrDefault(b => b.Name == createBookableObjectRequest.Name);
+        dbObject.Should().BeEquivalentTo(new BookableObject
+        {
+            Id = int.Parse(bookableObjectId),
+            Name = "Bookable Object Test Object 1",
+            Description = "Bookable Object Test Object 1",
+            FloorplanObjectId = "1",
+            AreaId = area.Id,
+            Area = area
+        }, "the data sent in the request should be added to the dataset");
+    }
+
+    [Test]
+    public async Task CreatingNewBookableObjectWithSameNameAsExistingBookableObjectReturns400()
+    {
+        //Arrange
+        var client = GetClient();
+        var location = await SetUpLocation("Bookable Object Test Location 1");
+        var area = await SetUpArea(location, "Bookable Object Test Area 1");
+        var bookableObject = await SetUpBookableObject(area, "Bookable Object Test Object 3");
+
+        var createBookableObjectRequest = new CreateBookableObjectRequest
+        {
+            Name = "Bookable Object Test Object 3",
+            Description = "Bookable Object Test Object 3",
+            FloorPlanObjectId = "1"
+        };
+
+        //Act
+        var response = await client.PostAsJsonAsync($"location/{location.Id}/area/{area.Id}/bookable-object",
+            createBookableObjectRequest);
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest, "bookable objects cannot share the same name");
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        responseContent.Should().Be("\"Bookable object with this name already exists in this area.\"",
+            "bookable objects cannot share the same name");
+
+        var dbBookableObject =
+            DbContext.BookableObjects.AsNoTracking().Where(b => b.Name == bookableObject.Name);
+        dbBookableObject.Should()
+            .HaveCount(1, "the duplicate bookable object should not have been added to the database");
+    }
+    
+    [Test]
+    public async Task UpdatingExistingBookableObjectIsSuccessful()
+    {
+        //Arrange
+        var client = GetClient();
+        var location = await SetUpLocation("Bookable Object Test Location 1");
+        var area = await SetUpArea(location, "Bookable Object Test Area 1");
+        var bookableObject = await SetUpBookableObject(area, "Bookable Object Test Object 2");
+
+        var updateBookableObjectRequest = new UpdateBookableObjectRequest
+        {
+            Name = "Bookable Object Test Object 2 - updated",
+            Description = "Bookable Object Test Object 2 - updated",
+            FloorPlanObjectId = "2"
+        };
+        
+        //Act
+        var response = await client.PutAsJsonAsync($"location/{location.Id}/area/{area.Id}/bookable-object/{bookableObject.Id}",
+            updateBookableObjectRequest);
+        
+        //Assert
+        response.EnsureSuccessStatusCode();
+
+        var dbBookableObject = DbContext.BookableObjects.AsNoTracking().SingleOrDefault(b => b.Id == bookableObject.Id);
+        dbBookableObject.Should().BeEquivalentTo(new BookableObject
+        {
+            Id = bookableObject.Id,
+            Name = "Bookable Object Test Object 2 - updated",
+            Description = "Bookable Object Test Object 2 - updated",
+            FloorplanObjectId = "2",
+            AreaId = area.Id
+        }, "updates sent in the request should be reflected in the database");
+    }
+}
--- src/api/tests/Hippo.Booking.Integration.Tests/Tests/LocationEndpointTests.cs
+++ src/api/tests/Hippo.Booking.Integration.Tests/Tests/LocationEndpointTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using FluentAssertions;
 using Hippo.Booking.Application.Commands.Location;
 using Hippo.Booking.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hippo.Booking.Integration.Tests.Tests;
 
@@ -10,7 +11,7 @@ namespace Hippo.Booking.Integration.Tests.Tests;
 public class LocationEndpointTests : IntegrationTestBase
 {
     [Test]
-    public async Task AddingANewUniqueLocationIsSuccessful()
+    public async Task CreatingANewUniqueLocationIsSuccessful()
     {
         // Arrange
         var client = GetClient();
@@ -41,7 +42,7 @@ public class LocationEndpointTests : IntegrationTestBase
     }
     
     [Test]
-    public async Task AddingALocationWithTheSameNameReturns400()
+    public async Task CreatingALocationWithTheSameNameReturns400()
     {
         // Arrange
         var client = GetClient();
@@ -59,12 +60,42 @@ public class LocationEndpointTests : IntegrationTestBase
         //Act
         var response = await client.PostAsJsonAsync("location", createLocation);
 
-        // Assert
+        //Assert
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest, "locations cannot share the same name");
         var responseContent = await response.Content.ReadAsStringAsync();
         responseContent.Should().Be("\"Location already exists\"", "locations cannot share the same name");
         
         var location = DbContext.Locations.Where(x => x.Name == "Test Location");
         location.Should().HaveCount(1, "the duplicate location should not have been added");
+    }
+
+    [Test]
+    public async Task UpdatingExistingLocationIsSuccessful()
+    {
+        //Arrange
+        var client = GetClient();
+        var location = await SetUpLocation("Location Test Location 1");
+        
+        var updateLocation = new UpdateLocationRequest
+        {
+            Name = "Location Test Location 1 - updated",
+            Description = "Location Test Location 1 - updated"
+        };
+        
+        //Act
+        var response = await client.PutAsJsonAsync("location/" + location.Id, updateLocation);
+        
+        //Assert
+        response.EnsureSuccessStatusCode();
+        
+        var dbLocation = await DbContext.Locations
+            .AsNoTracking()
+            .SingleOrDefaultAsync(l => l.Id == location.Id);
+        dbLocation.Should().BeEquivalentTo(new Location
+        {
+            Id = location.Id,
+            Name = "Location Test Location 1 - updated",
+            Description = "Location Test Location 1 - updated"
+        }, "the updates to the location should be reflected in the database");
     }
 }
