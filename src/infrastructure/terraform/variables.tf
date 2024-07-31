08aab5fec4018497ea42cd2f9f3f251ac0e71f2c
Harry Young <harry.young@hippodigital.co.uk>
2024-07-31 13:00:46 +0100

    Fix terraform build steps

--- src/infrastructure/terraform/variables.tf
+++ src/infrastructure/terraform/variables.tf
@@ -1,11 +1,13 @@
 variable "aws_access_key_id" {
   description = "AWS Access Key ID"
   type        = string
+  sensitive   = true
 }
 
 variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
   type        = string
+  sensitive   = true
 }
 
 variable "s3_terraform_bucket" {
