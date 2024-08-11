0ec1950516a918e646356613dde5f0fd4f3b28aa
ClarksonAdam <adam.clarkson@hippodigital.co.uk>
2024-08-11 12:41:17 +0100

    feat: wip booking page

--- src/web/hippo-booking/src/pages/locations/booking/Booking.tsx
+++ src/web/hippo-booking/src/pages/locations/booking/Booking.tsx
@@ -11,7 +11,7 @@ import { CtaButton } from "../../../components/buttons/Buttons";
 
 import "react-datepicker/dist/react-datepicker.css";
 import './Booking.scss';
-import DatePicker from "react-datepicker";
+import CustomDatePicker from "../../../components/date-picker/DatePicker";
 
 // Seperate API endpoints just for the floorplan? then it can be cached for a long time on both server and client for optimal performance. If so change floorplan as well
 // Desk data can be fetched from the booking API and we can switch days without reloading the floorplan.
@@ -268,7 +268,7 @@ const DeskBooking = () => {
     return bookedBy ? `Booked by: ${bookedBy}` : null;
   };
 
-  const adjustDate = (direction: 'next' | 'previous' | 'today') => {
+  const adjustDate = (direction: 'next' | 'previous' | 'today' | 'tomorrow') => {
     setSelectedDate(prev => {
       if(direction === 'today') {
         return new Date();
@@ -288,39 +288,9 @@ const DeskBooking = () => {
   return (
     <div>
       <h1>Desk Booking</h1>
+      <CustomDatePicker adjustDate={adjustDate} inputOnChange={(date) => setSelectedDate(date!)} selectedDate={selectedDate}/>
       <div>
-        Choose a date: <DatePicker 
-          showIcon 
-          selected={selectedDate} 
-          onChange={(date) => setSelectedDate(date!)}
-          icon={<svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 48 48"
-          >
-            <mask id="ipSApplication0">
-              <g fill="none" stroke="#fff" strokeLinejoin="round" strokeWidth="4">
-                <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
-                <path
-                  fill="#fff"
-                  d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
-                ></path>
-              </g>
-            </mask>
-            <path
-              fill="currentColor"
-              d="M0 0h48v48H0z"
-              mask="url(#ipSApplication0)"
-            ></path>
-          </svg>} />
-      </div>
-      <div>
-      <button type="button" onClick={() =>  adjustDate('previous')}>Previous</button>
-        <button type="button" onClick={() => adjustDate('today')}>
-          Today
-        </button>
-        <button type="button" onClick={() => adjustDate('next')}>Next</button>
+
       </div>
         <br />
       <button type="button" onClick={handleToggleCanvas}>
