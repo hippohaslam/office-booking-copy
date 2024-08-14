5874d10b19759dd755b4500825ca9da212409549
Harry Young <harry.young@hippodigital.co.uk>
2024-08-14 08:03:38 +0100

    All Nuget packages up to date and fix build warning

--- src/api/Hippo.Booking.Application/Queries/Users/UserListResponse.cs
+++ src/api/Hippo.Booking.Application/Queries/Users/UserListResponse.cs
@@ -2,7 +2,7 @@ namespace Hippo.Booking.Application.Queries.Users;
 
 public class UserListResponse
 {
-    public string Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 
     public string Email { get; set; } = string.Empty;
 
