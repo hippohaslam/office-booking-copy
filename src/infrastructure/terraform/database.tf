62944f9d16cb8e36644874c8ea0f7d2ebc2f57e6
Harry Young <harry.young@hippodigital.co.uk>
2024-08-14 12:41:47 +0100

    Lock down database

--- src/infrastructure/terraform/database.tf
+++ src/infrastructure/terraform/database.tf
@@ -68,6 +68,16 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   from_port         = 5432
   ip_protocol       = "tcp"
   to_port           = 5432
+  description       = "Internal traffic in VPC"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_hippo_office_ipv4" {
+  security_group_id = aws_security_group.db-security-group.id
+  cidr_ipv4         = "195.224.212.222/32"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+  description       = "Leeds Hippo office"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
@@ -80,23 +90,4 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.db-security-group.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-
-########## Only for development ##########
-
-resource "aws_vpc_security_group_ingress_rule" "allow_all_ipv4_inbound" {
-  security_group_id = aws_security_group.db-security-group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 5432
-  ip_protocol       = "tcp"
-  to_port           = 5432
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_all_ipv6_inbound" {
-  security_group_id = aws_security_group.db-security-group.id
-  cidr_ipv6         = "::/0"
-  from_port         = 5432
-  ip_protocol       = "tcp"
-  to_port           = 5432
 }
