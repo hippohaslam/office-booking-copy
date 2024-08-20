422e25542b031e943d77ce10e1dd32fcc35fc650
ClarksonAdam <adam.clarkson@hippodigital.co.uk>
2024-08-20 22:48:34 +0100

    feat: modal animations

--- src/web/hippo-booking/src/components/modals/confirm/ConfirmModal.tsx
+++ src/web/hippo-booking/src/components/modals/confirm/ConfirmModal.tsx
@@ -17,7 +17,8 @@ type BookingCancelModalProps = {
 
 const BookingCancelModal = ({title, isOpen, childElement, showConfirmButton, onConfirm, confirmButtonLabel, confirmButtonColor = 'cta-green', onCancel, cancelButtonLabel, cancelButtonColor = 'cta-red'} : BookingCancelModalProps) => {
     const firstButtonRef = useRef<HTMLButtonElement>(null);
-    const overlayRef = useRef(null);
+    const overlayRef = useRef<HTMLDivElement>(null);
+    const modalRef = useRef<HTMLDivElement>(null);
     
     useEffect(() => {
         if (isOpen) {
@@ -27,15 +28,28 @@ const BookingCancelModal = ({title, isOpen, childElement, showConfirmButton, onC
         }
     }, [isOpen]);
 
+    const closeModal = () => {
+        if (modalRef.current) {
+            modalRef.current.classList.add("out");
+        }
+        if (overlayRef.current) {
+            overlayRef.current.classList.add("out");
+        }
+        // Timeout 200 is for the duration of exit animation
+        setTimeout(() => {
+            onCancel();
+        }, 200);
+    }
+
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (overlayRef.current && (overlayRef.current as HTMLElement).contains(event.target as Node) && !document.getElementById("cancel-booking-modal")?.contains(event.target as Node)) {
-                onCancel();
+                closeModal();
             }
         };
         const handleEscapeKey = (event: KeyboardEvent) => {
             if (event.key === 'Escape'){
-                onCancel();
+                closeModal();
             }
         }
 
@@ -53,13 +67,13 @@ const BookingCancelModal = ({title, isOpen, childElement, showConfirmButton, onC
     if (!isOpen) return null;
     return (
         <div className="modal-overlay" ref={overlayRef} data-testid="modal-page-overlay">
-            <div className="modal-booking" id="cancel-booking-modal" role="alertdialog" aria-labelledby="cancel-modal-heading" aria-describedby="cancel-modal-booking-info" aria-modal="true">
+            <div className="modal-booking" ref={modalRef} id="cancel-booking-modal" role="alertdialog" aria-labelledby="cancel-modal-heading" aria-describedby="cancel-modal-booking-info" aria-modal="true">
                 <h2 id="cancel-modal-heading">{title}</h2>
                 {childElement}
                 {showConfirmButton ? (
                     <CtaButton ref={firstButtonRef} text={confirmButtonLabel ?? ""} color={confirmButtonColor} onClick={onConfirm} />
                 ) : null}
-                <CtaButton text={cancelButtonLabel} color={cancelButtonColor} onClick={onCancel} />
+                <CtaButton text={cancelButtonLabel} color={cancelButtonColor} onClick={closeModal} />
             </div>
         </div>
     )
