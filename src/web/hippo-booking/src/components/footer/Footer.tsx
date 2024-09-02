2a263e6210e888507eeebb85f96fe8b7f745722e
Harry Young <harry.young@hippodigital.co.uk>
2024-09-02 12:45:34 +0100

    Small tidy ups to docs

--- src/web/hippo-booking/src/components/footer/Footer.tsx
+++ src/web/hippo-booking/src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <div className='footer-container'>
         <p>Hippo booking - Version 0.1</p>
-        <a href='https://github.com/harryy94/hippo-booking' target='_blank'>
+        <a href='https://github.com/hippo-digital/office-booking' target='_blank'>
           View this project on GitHub
         </a>
       </div>
