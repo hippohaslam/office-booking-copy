75fb0f10a6a4d2d136d86e74bcbdcccd9fb13195
Harry Young <harry.young@hippodigital.co.uk>
2024-09-15 17:24:38 +0100

    "Sleep" mode on screen availability

--- src/api/Hippo.Booking.API/Endpoints/ScreenEndpoints.cs
+++ src/api/Hippo.Booking.API/Endpoints/ScreenEndpoints.cs
@@ -1,6 +1,8 @@
 using Hippo.Booking.Application.Queries.Bookings;
 using Hippo.Booking.Core.Enums;
+using Hippo.Booking.Core.Interfaces;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Hippo.Booking.API.Endpoints;
 
@@ -8,9 +10,19 @@ public class ScreenEndpoints() : EndpointBase("screen", "Screen", AccessLevelEnu
 {
     protected override void MapEndpoints(RouteGroupBuilder builder)
     {
-        builder.MapGet("availablity/{bookableObjectId}",
-            async Task<Results<Ok<BookableObjectBookingStateResponse>, BadRequest<string>>> (IBookingQueries bookingQueries, int bookableObjectId) =>
+        builder.MapGet("availability/{bookableObjectId}",
+            async Task<Results<Ok<BookableObjectBookingStateResponse>, NoContent, BadRequest<string>>> 
+                (IBookingQueries bookingQueries, IDateTimeProvider dateTimeProvider, int bookableObjectId) =>
             {
+                var now = dateTimeProvider.Now;
+
+                if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday ||
+                    now.Hour < 7 || now.Hour >= 19)
+                {
+                    // Outside of working hours, so this will instruct the screen to sleep
+                    return TypedResults.NoContent();
+                }
+
                 var bookingState = await bookingQueries.GetBookedState(bookableObjectId);
 
                 return TypedResults.Ok(bookingState);
