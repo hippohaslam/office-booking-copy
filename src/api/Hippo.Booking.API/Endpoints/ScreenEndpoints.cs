d0b01593b09360983bf95c8d84a49b1b327f29ad
Harry Young <harry.young@hippodigital.co.uk>
2025-03-05 11:02:25 +0000

    Add screen auth key

--- src/api/Hippo.Booking.API/Endpoints/ScreenEndpoints.cs
+++ src/api/Hippo.Booking.API/Endpoints/ScreenEndpoints.cs
@@ -1,6 +1,7 @@
 using Hippo.Booking.Application.Queries.Bookings;
 using Hippo.Booking.Core.Enums;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hippo.Booking.API.Endpoints;
 
@@ -9,9 +10,19 @@ public class ScreenEndpoints() : EndpointBase("screen", "Screen", AccessLevelEnu
     protected override void MapEndpoints(RouteGroupBuilder builder)
     {
         builder.MapGet("availability/{bookableObjectId}",
-            async Task<Results<Ok<BookableObjectBookingStateResponse>, BadRequest<string>>> 
-                (IBookingQueries bookingQueries, int bookableObjectId) =>
+            async Task<Results<Ok<BookableObjectBookingStateResponse>, BadRequest<string>, NotFound>>
+            (
+                IBookingQueries bookingQueries,
+                int bookableObjectId,
+                [FromHeader(Name = "Auth-Key")] string authKey,
+                IConfiguration configuration
+            ) =>
             {
+                if (authKey != configuration["Screen:AuthKey"])
+                {
+                    return TypedResults.NotFound();
+                }
+
                 var bookingState = await bookingQueries.GetBookedState(bookableObjectId);
 
                 return TypedResults.Ok(bookingState);
--- src/infrastructure/terraform/secrets.tf
+++ src/infrastructure/terraform/secrets.tf
@@ -9,7 +9,13 @@ resource "aws_secretsmanager_secret_version" "dotnet_secrets_version" {
     "ConnectionStrings:HippoBookingDbContext": "Server=${aws_db_instance.hippo-booking-db.address};Port=${aws_db_instance.hippo-booking-db.port};User Id=${aws_db_instance.hippo-booking-db.username};Password=${aws_db_instance.hippo-booking-db.password};Database=HippoBooking_${var.environment}",
     "Google:ClientSecret": "${var.google_client_secret}",
     "Slack:Token": "${var.slack_token}",
-    "Slack:SigningSecret": "${var.slack_signing_secret}"
+    "Slack:SigningSecret": "${var.slack_signing_secret}",
+    "Screen:AuthKey: " "${random_password.screen_auth_key.result}"
 }
 EOF
+}
+
+resource "random_password" "screen_auth_key" {
+  length = 16
+  special = false
 }
