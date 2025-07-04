7c549b5a9285426d88bd9e03ca0db37267116339
Chris Haslam <chris.haslam@hippodigital.co.uk>
2025-07-04 16:21:52 +0100

    Generated workflow and revert eslint.config

--- src/web/hippo-booking/eslint.config.cjs
+++ src/web/hippo-booking/eslint.config.cjs
@@ -1,30 +1,11 @@
-const globals = require("globals");
-
-module.exports = [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2020,
-      },
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react-hooks/recommended"
-    ],
-    plugins: ["react-refresh"],
-    rules: {
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    }
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
-  {
-    ignores: ["dist", ".eslintrc.cjs"]
-  }
-];
+};
