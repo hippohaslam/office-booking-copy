15b4ae68fe52ee8ad3c0969c5d13879bc3b93acc
Chris Haslam <chris.haslam@hippodigital.co.uk>
2025-07-04 15:53:09 +0100

    Update eslint

--- src/web/hippo-booking/eslint.config.cjs
+++ src/web/hippo-booking/eslint.config.cjs
@@ -1,10 +1,30 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+const globals = require("globals");
+
+module.exports = [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended"
+    ],
+    plugins: ["react-refresh"],
+    rules: {
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    }
   },
-};
+  {
+    ignores: ["dist", ".eslintrc.cjs"]
+  }
+];
